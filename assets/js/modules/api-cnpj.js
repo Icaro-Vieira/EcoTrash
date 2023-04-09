564fa8e905a237bc363f5edf3e219c25a526991b
Icaro Vieira <icarogbov@gmail.com>
2023-04-09 14:15:11 -0300

    Começando Mascara dos campos

--- assets/js/modules/api-cnpj.js
+++ assets/js/modules/api-cnpj.js
@@ -1,9 +1,21 @@
 export default function apiCnpj() {
   // campo onde o usuário insere o CNPJ
   const inputCnpj = document.querySelector("#cnpj");
+
+  // Função para fazer a máscara do CNPJ
+  // function maskCNPJ(inputCnpj) {
+  //   return inputCnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5');
+  // }
+
+  // maskCNPJ(inputCnpj)
   
+  // function removeMask(inputCnpj) {
+  //   return inputCnpj.replace(/[^\d]+/g, '');
+  // }
+
   // Quando o usuário preencher o campo de CNPJ e mudar de foco, ele vai fazer a consulta na API
   inputCnpj.addEventListener("blur", function (event) {
+    // const cnpj = removeMask(event.target.value);
     const cnpj = event.target.value;
 
     fetch(`https://api-publica.speedio.com.br/buscarcnpj?cnpj=${cnpj}`)
@@ -20,6 +32,7 @@ export default function apiCnpj() {
         document.querySelector("#bairro").value = cnpjFields["BAIRRO"];
         document.querySelector("#cidade").value = cnpjFields["MUNICIPIO"];
         document.querySelector("#numero").value = cnpjFields["NUMERO"];
+        document.querySelector("#estado").value = cnpjFields["UF"];
       });
   });
 }
